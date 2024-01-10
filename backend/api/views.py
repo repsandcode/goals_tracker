94999d945f2619ea3f201ea14a9622d894f3801b
repsandcode <albertjonathan.dev@gmail.com>
2024-01-11 02:07:50 +0800

    update backend and add jwt auth

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
@@ -6,14 +6,60 @@ from rest_framework.viewsets import ModelViewSet
 from base.models import User, TopGoal, DailyGoal
 from .serializers import UserSerializer, TopGoalSerializer, DailyGoalSerializer
 
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        # Add custom claims
+        token['username'] = user.username
+        # ...
+
+        return token
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
 class UserViewSet(ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
+    def list(self, request):
+        serializer = UserSerializer(self.queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        item = get_object_or_404(self.queryset, pk=pk)
+        serializer = UserSerializer(item)
+        return Response(serializer.data)
+
+
 class TopGoalViewSet(ModelViewSet):
     queryset = TopGoal.objects.all()
     serializer_class = TopGoalSerializer
+    
+    def list(self, request):
+        serializer = TopGoalSerializer(self.queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        item = get_object_or_404(self.queryset, pk=pk)
+        serializer = TopGoalSerializer(item)
+        return Response(serializer.data)
+
 
 class DailyGoalViewSet(ModelViewSet):
     queryset = DailyGoal.objects.all()
     serializer_class = DailyGoalSerializer
+    
+    def list(self, request):
+        serializer = DailyGoalSerializer(self.queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        item = get_object_or_404(self.queryset, pk=pk)
+        serializer = DailyGoalSerializer(item)
+        return Response(serializer.data)
