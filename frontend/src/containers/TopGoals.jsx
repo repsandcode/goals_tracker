662295b671192a974736ecaa2c89c27e503797f2
repsandcode <albertjonathan.dev@gmail.com>
2024-01-19 19:24:17 +0800

    initializing...

--- frontend/src/containers/TopGoals.jsx
+++ frontend/src/containers/TopGoals.jsx
@@ -6,11 +6,148 @@ import axios from "axios";
 import { IoAdd } from "react-icons/io5";
 import TopGoalBox from "../components/TopGoalBox";
 
-const TopGoals = () => {
-  const { authTokens, logout } = useAuth();
-  const [topGoals, setTopGoals] = useState([]);
+const TopGoalForm = ({ authTokens }) => {
+  const [topGoal, setTopGoal] = useState("");
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [errorMsgs, setErrorMsgs] = useState({
+    topGoal: "",
+    startDate: "",
+    endDate: "",
+  });
 
+  const onChangeTopGoal = (e) => setTopGoal(e.target.value);
+  const onChangeStartDate = (e) => setStartDate(e.target.value);
+  const onChangeEndDate = (e) => setEndDate(e.target.value);
+
+  const handleAddTopGoal = async (e) => {
+    e.preventDefault();
+
+    if (topGoal === "") {
+      setErrorMsgs((prev) => ({
+        ...prev,
+        topGoal: "Input your desired goal",
+      }));
+    }
+
+    if (startDate === "") {
+      setErrorMsgs((prev) => ({
+        ...prev,
+        startDate: "Enter a start date",
+      }));
+    }
+
+    if (endDate === "") {
+      setErrorMsgs((prev) => ({
+        ...prev,
+        endDate: "Enter an end date",
+      }));
+    }
+
+    if (errorMsgs.topGoal || errorMsgs.startDate || errorMsgs.endDate) {
+      return;
+    }
+
+    setLoading(true);
+
+    try {
+      const response = await axios.post(
+        "http://127.0.0.1:8000/api/top-goals/",
+        {
+          name: topGoal,
+          start_date: startDate,
+          end_date: endDate,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${authTokens.access}`, // Include your authentication token here
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      console.log(response);
+      console.log(response.data);
+    } catch (error) {
+      console.error("Login failed:", error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <form method="dialog" onSubmit={handleAddTopGoal}>
+      <div className="mt-5">
+        <label
+          htmlFor="topGoal"
+          className="block mb-2 text-sm font-medium text-black"
+        >
+          Top Goal
+        </label>
+        <input
+          type="text"
+          name="topGoal"
+          id="topGoal"
+          className="sm:text-sm rounded-lg block w-full p-2.5 outline-none"
+          placeholder="Create a micro-SAAS product"
+          value={topGoal}
+          onChange={onChangeTopGoal}
+        />
+      </div>
+
+      <div className="flex gap-x-2.5 mt-5">
+        <div className="grow">
+          <label
+            htmlFor="startDate"
+            className="block mb-2 text-sm font-medium text-black"
+          >
+            Start Date
+          </label>
+          <input
+            type="date"
+            name="startDate"
+            id="startDate"
+            className="sm:text-sm rounded-lg block w-full p-2.5 outline-none"
+            value={startDate}
+            onChange={onChangeStartDate}
+          />
+        </div>
+
+        <div className="grow">
+          <label
+            htmlFor="endDate"
+            className="block mb-2 text-sm font-medium text-black"
+          >
+            End Date
+          </label>
+          <input
+            type="date"
+            name="endData"
+            id="endDate"
+            className="sm:text-sm rounded-lg block w-full p-2.5 outline-none"
+            value={endDate}
+            onChange={onChangeEndDate}
+          />
+        </div>
+      </div>
+
+      {/* if there is a button in form, it will close the modal */}
+      <div className="modal-action">
+        <button className="btn bg-red-400">Close</button>
+        <button type="submit" className="btn bg-green-400">
+          {loading ? "Adding..." : "Add Top Goal"}
+        </button>
+      </div>
+    </form>
+  );
+};
+
+const TopGoals = () => {
   const navigate = useNavigate();
+  const { authTokens, logout } = useAuth();
+
+  const [topGoals, setTopGoals] = useState([]);
 
   useEffect(() => {
     getTopGoals();
@@ -58,21 +195,12 @@ const TopGoals = () => {
             <span className="ml-1">Add</span>
           </button>
 
-          <dialog
-            id="addTopGoal"
-            className="modal modal-bottom sm:modal-middle"
-          >
+          <dialog id="addTopGoal" className="modal modal-middle">
             <div className="modal-box">
-              <h3 className="font-bold text-lg">Hello!</h3>
-              <p className="py-4">
-                Press ESC key or click the button below to close
-              </p>
-              <div className="modal-action">
-                <form method="dialog">
-                  {/* if there is a button in form, it will close the modal */}
-                  <button className="btn">Close</button>
-                </form>
-              </div>
+              <h3 className="font-bold text-lg">
+                What's a top goal you want to achieve?
+              </h3>
+              <TopGoalForm authTokens={authTokens} />
             </div>
           </dialog>
         </div>
