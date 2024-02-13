8862eee605c098bba5b6333c88771f30ff400785
repsandcode <albertjonathan.dev@gmail.com>
2024-02-13 09:19:59 +0800

    add login and logout views, and urls

--- base/goals_tracker/urls.py
+++ base/goals_tracker/urls.py
@@ -1,6 +1,21 @@
 from django.urls import path
 from . import views
 
+app_name = "goals_tracker"
+
 urlpatterns = [
-  path("<str:name>", views.index, name="index")
+  # home
+  path("<str:username>", views.index, name="index"),
+  # path("<str:username>/create-big-goal", views.create_biggoal, name="create_biggoal"),
+
+  # big goal page
+  # path("<str:username>/<str:biggoal>", views.biggoal, name="biggoal"),
+  # path("<str:username>/<str:biggoal>/create-checkpoint-goals", views.create_checkpointgoal, name="create_checkpointgoal"),
+  # path("<str:username>/<str:biggoal>/create-daily-systems", views.create_dailysystem, name="create_dailysystem"),
+  # path("<str:username>/<str:biggoal>/create-anti-goals", views.create_antigoals, name="create_antigoals"),
+
+  # header
+  path("login", views.login, name="login"),
+  path("logout", views.logout, name="logout"),
+  # path("register", views.register, name="register"),  
 ]
--- base/goals_tracker/views.py
+++ base/goals_tracker/views.py
@@ -1,8 +1,36 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+import json
+
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
-def index(request, name):
+def index(request, username):
   return render(request, "goals_tracker/index.html", {
-    "name": name.capitalize()
+    "username": username.capitalize()
   })
+
+
+def login(request):
+  if request.method == "POST":
+    # Attempt to sign user in
+    username = request.POST["username"]
+    password = request.POST["password"]
+    user = authenticate(request, username=username, password=password)
+      
+    # Check if authentication successful
+    if user is not None:
+      login(request, user)
+      return HttpResponseRedirect(reverse("index"))
+    else:
+      return render(request, "goals_tracker/login.html", {
+        "message": "Invalid username and/or password."
+      })
+  else:
+    return render(request, "goals_tracker/login.html")
+
+
+def logout(request):
+  logout(request)
+  return HttpResponseRedirect(reverse("login"))
