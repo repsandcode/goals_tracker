31c27c2c06850a3900f28a7cc2ba3f877e1e4530
repsandcode <albertjonathan.dev@gmail.com>
2024-02-22 07:10:54 +0800

    fix user model alter bug

--- base/goals_tracker/models.py
+++ base/goals_tracker/models.py
@@ -1,15 +1,9 @@
-from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 class User(AbstractUser):
-    # Define related_name for groups field
-    groups = models.ManyToManyField('auth.Group', related_name="user_groups_%(class)s")
-    
-    # Define related_name for user_permissions field
-    user_permissions = models.ManyToManyField('auth.Permission', related_name="user_permissions_set_%(class)s")
-
-    bio = models.TextField(blank=True)
-    points = models.IntegerField(default=0)
-
-    def __str__(self):
-        return self.username
+  bio = models.TextField(blank=True)
+  points = models.IntegerField(default=0)
+  
+  def __str__(self):
+    return self.username
