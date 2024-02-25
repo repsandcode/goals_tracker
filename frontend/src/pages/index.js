073e90d37289d9ffb4b1e54792f69001a94603b3
repsandcode <albertjonathan.dev@gmail.com>
2024-02-25 08:40:59 +0800

    remove frontend folder

--- frontend/src/pages/index.js
+++
@@ -1,6 +0,0 @@
-import HomePage from "./HomePage";
-import LoginPage from "./LoginPage";
-import RegisterPage from "./RegisterPage";
-import TopGoalPage from "./TopGoalPage";
-
-export { HomePage, LoginPage, RegisterPage, TopGoalPage };
