a581af7dfd9f394bf9beba20fef61cb0a14592ff
repsandcode <albertjonathan.dev@gmail.com>
2024-02-29 02:43:01 +0800

    add big goal modal form

--- base/goals_tracker/static/goals_tracker/main.js
+++ base/goals_tracker/static/goals_tracker/main.js
@@ -1,13 +1,38 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // invoke greeting
+  // show welcome banner
   addGreeting();
 
-  document
-    .querySelector("#show-big-goal-form")
-    .addEventListener("click", () => showBigGoalForm());
-});
+  // modals
+  const modalCenter = document.querySelector(".modal-center");
+  const bigGoalModal = document.querySelector("#big-goal-modal");
 
-const showBigGoalForm = () => {
-  console.log("clicked");
-  document.querySelector("#big-goal-form").classList.toggle("d-none");
-};
+  // function to show the modal
+  const showBigGoalModal = () => {
+    console.log("clicked");
+    bigGoalModal.style.display = "block";
+  };
+
+  // function to hide the modal
+  const hideBigGoalModal = () => {
+    bigGoalModal.style.display = "none";
+  };
+
+  // attach event listener to the button
+  document
+    .querySelector("#open-big-goal-form")
+    .addEventListener("click", () => showBigGoalModal());
+
+  // attach event listener to the close button
+  document
+    .querySelector(".close")
+    .addEventListener("click", () => hideBigGoalModal());
+
+  // any clicks
+  window.onclick = function (event) {
+    // when the user clicks anywhere outside of the modal or its child elements, close it
+    if (event.target == modalCenter) {
+      console.log("click");
+      hideBigGoalModal();
+    }
+  };
+});
