18d2cefa4fa8eca7be2fa2588b4301b23cdfea61
repsandcode <albertjonathan.dev@gmail.com>
2024-03-31 10:24:08 +0800

    intializing modals

--- base/goals_tracker/static/goals_tracker/big_goal.js
+++ base/goals_tracker/static/goals_tracker/big_goal.js
@@ -1,8 +1,31 @@
-// checkpoint goal form
-const checkpointGoalModal = document.querySelector("#checkpoint-goal-modal");
-
 // daily system form
 const dailySystemModal = document.querySelector("#daily-system-modal");
-
+// checkpoint goal form
+const checkpointGoalModal = document.querySelector("#checkpoint-goal-modal");
 // anti goals form
 const antiGoalModal = document.querySelector("#anti-goal-modal");
+
+const showDailySystemModal = () => {
+  dailySystemModal.style.display = "block";
+  document.querySelector("#daily-system-form").onsubmit = createDailySystem;
+};
+const hideDailySystemModal = () => {
+  dailySystemModal.style.display = "none";
+};
+
+const showCheckpointGoalModal = () => {
+  checkpointGoalModal.style.display = "block";
+  document.querySelector("#checkpoint-goal-form").onsubmit =
+    createCheckpointGoal;
+};
+const hideCheckpointGoalModal = () => {
+  checkpointGoalModal.style.display = "none";
+};
+
+const showAntiGoalModal = () => {
+  antiGoalModal.style.display = "block";
+  document.querySelector("#anti-goal-form").onsubmit = createAntiGoal;
+};
+const hideAntiGoalModal = () => {
+  antiGoalModal.style.display = "none";
+};
