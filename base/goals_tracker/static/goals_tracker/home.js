328e534356559cfd718562fe038ac96050297a51
repsandcode <albertjonathan.dev@gmail.com>
2024-07-22 21:02:14 +0800

    update structure again

--- base/goals_tracker/static/goals_tracker/home.js
+++ base/goals_tracker/static/goals_tracker/home.js
@@ -165,8 +165,8 @@ const getUserData = () => {
           profile_header.innerHTML = 
             `
               <p class="mb-0 fs-4">${user.first_name !== "" ? `${user.first_name} ${user.last_name}` : user.username}</p>
-              <p class="mb-0 fw-light fs-6">${user.email}</p>
-            `;
+              `;
+              // <p class="mb-0 fw-light fs-6">${user.email}</p>
         }
       })
   } catch (error) {
