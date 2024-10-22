48b17b8553e0741b5a93d66bdc334ffb635b3cfe
repsandcode <albertjonathan.dev@gmail.com>
2024-10-22 15:22:35 +0800

    ready to deploy baby

--- base/base/settings.py
+++ base/base/settings.py
@@ -94,7 +94,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
